3ef4a218becd482113c16b109978d090606c6f8b
Edracoon <77042040+Edracoon@users.noreply.github.com>
2021-03-02 17:19:10 +0100

    final commit + comment

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: epfennig <epfennig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/07 16:06:55 by marvin            #+#    #+#             */
-/*   Updated: 2021/03/02 17:17:43 by epfennig         ###   ########.fr       */
+/*   Updated: 2021/03/02 17:19:00 by epfennig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,18 +109,18 @@ int				get_next_line(int fd, char **line)
 		temp = ft_strjoin(str, buff);                                         /* strjoin de la static str avec la ligne lue, qu'on stock dans temp le temps de free l'ancienne adresse de str */
 		ft_strfree(&str);                                                     /* on free l'adresse de str pour ensuite lui reassigner temp pour ne pas avoir de leaks et de superposition de string */
 		str = temp;                                                           /* assignation de str avec le nouvel strjoin */
-	}                                                                       /* on check le ret a la sortie de la boucle, on sort si ret <= 0 ou si l'on a trouvé le '\n' */
-	if (ret == 0)                                                           /* si ret = 0 , cela veut dire que il n'y a plus rien a lire car ret = 0*/
+	}                                                                           /* on check le ret a la sortie de la boucle, on sort si ret <= 0 ou si l'on a trouvé le '\n' */
+	if (ret == 0)                                                               /* si ret = 0 , cela veut dire que il n'y a plus rien a lire car ret = 0*/
 		*line = ft_strdup(str);                                               /* on copy + malloc dans *line qui est notre ligne finale de retour, on fait strdup car on a l'EOF donc on a la len qu'il nous faut*/
-	else if (ret > 0)                                                       /* si ret > 0 cela veut dire qu'on n'est pas arrivé a l'EOF donc on fait un substr*/
+	else if (ret > 0)                                                           /* si ret > 0 cela veut dire qu'on n'est pas arrivé a l'EOF donc on fait un substr*/
 		*line = ft_substr(str, 0, (ft_strchr(str, '\n') - str));              /* strchr - str c'est adresse d'un pointeur - l'adresse de l'autre et ca fait un int : on part de start = 0 et len = la position du '\n' */
 	else
 		return (-1);                                                          /* else on trouve une erreur dans le ret (ret < 0) */
-	temp = ft_strdup(str + (ft_strlen(*line) + ((ret > 0) ? +1 : +0)));     /* partie on l'on stoque le reste pour la suite di programme */
-	ft_strfree(&str);                                                       /* on free une derniere fois l'adresse de str */
+	temp = ft_strdup(str + (ft_strlen(*line) + ((ret > 0) ? +1 : +0)));       /* partie on l'on stoque le reste pour la suite di programme */
+	ft_strfree(&str);                                                         /* on free une derniere fois l'adresse de str */
 	str = temp;
-	return (ret == 0 ? 0 * ft_strfree(&str) : 1);                           /* valeur de return, on reverifie le ret de read, si on est a l'EOF, on free &str tout en renvoyant 0 */
-}                                                                          /* sinon on return 1 == une ligne a été lu */
+	return (ret == 0 ? 0 * ft_strfree(&str) : 1);                             /* valeur de return, on reverifie le ret de read, si on est a l'EOF, on free &str tout en renvoyant 0 */
+}                                                                             /* sinon on return 1 == une ligne a été lu */
 
 /*
 ** Main Tester :
